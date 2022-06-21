094372fd0bfef04152d91fadb0e315d11066448d
mbs-code <mbscodec@gmail.com>
2022-06-22 00:51:00 +0900

    fmt: lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
-import { defineNuxtConfig } from "nuxt";
-import eslintPlugin from 'vite-plugin-eslint';
+import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  srcDir: "src/",
+  srcDir: 'src/',
   ssr: false,
-  target: "static",
+  target: 'static',
 
   vite: {
     plugins: [eslintPlugin({
       fix: true,
     })]
   },
-});
+})
