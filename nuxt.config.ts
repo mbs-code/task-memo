c31b3482a803e9611bb965c8066a7b0f6ed936ec
mbs-code <mbscodec@gmail.com>
2022-06-22 20:57:27 +0900

    add: vue devtool の実装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 import { defineNuxtConfig } from 'nuxt'
 import eslintPlugin from 'vite-plugin-eslint'
 
+const isProduct = process.env.NODE_ENV === 'production'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src/',
   ssr: false,
   target: 'static',
+
+  app: {
+    head: {
+      script: [
+        !isProduct && { src: 'http://localhost:8098' }, // vue devtools
+      ],
+    },
+  },
 
   vite: {
     plugins: [eslintPlugin({
