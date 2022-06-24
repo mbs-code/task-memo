070c2fc53539494a29e4b297dfb0a06e1e12ee9e
mbs-code <mbscodec@gmail.com>
2022-06-25 02:19:25 +0900

    add: tag API を実装

--- src/app.vue
+++ src/app.vue
@@ -8,27 +8,90 @@
 </template>
 
 <script setup lang="ts">
+import { db, migrator, Database } from '~~/src/databases/db'
 import { useReportAPI } from '~~/src/apis/useReportAPI'
+import { useTagAPI } from '~~/src/apis/useTagAPI'
 
 onMounted(async () => {
+  await Database.dbWipe()
+  await migrator.migrateToLatest()
+
+  const tagAPI = useTagAPI()
   const reportAPI = useReportAPI()
+  await tagAPI.clear()
   await reportAPI.clear()
 
-  await reportAPI.create({
-    text: 'text',
-    tagNames: ['tag'],
+  const a = await tagAPI.create({
+    name: 'a',
+    is_pinned: false,
+    priority: 0,
   })
 
-  const report = await reportAPI.create({
-    text: 'テキスト',
-    tagNames: ['タグA', 'タグB'],
-  })
-  await reportAPI.update(report.id, {
-    text: report.text,
-    tagNames: report.tags.map(t => t.name)
+  const b = await tagAPI.create({
+    name: 'b',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: a.id,
   })
 
-  const reports = await reportAPI.getAll()
-  console.log(reports)
+  const c = await tagAPI.create({
+    name: 'c',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: a.id,
+  })
+
+  const d = await tagAPI.create({
+    name: 'd',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: c.id,
+  })
+
+  const e = await tagAPI.create({
+    name: 'e',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: c.id,
+  })
+
+  const z = await tagAPI.create({
+    name: 'z',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: e.id,
+  })
+
+  await tagAPI.update(e.id, {
+    name: 'e',
+    is_pinned: false,
+    priority: 0,
+    parent_tag_id: b.id,
+  })
+
+  console.log('----------')
+
+  const rem = await tagAPI.remove(c.id)
+  console.log(rem)
+
+  const tags = await tagAPI.getAll()
+  console.log(tags)
+
+  // await reportAPI.create({
+  //   text: 'text',
+  //   tagNames: ['tag'],
+  // })
+
+  // const report = await reportAPI.create({
+  //   text: 'テキスト',
+  //   tagNames: ['タグA', 'タグB'],
+  // })
+  // await reportAPI.update(report.id, {
+  //   text: report.text,
+  //   tagNames: report.tags.map(t => t.name)
+  // })
+
+  // const reports = await reportAPI.getAll()
+  // console.log(reports)
 })
 </script>
